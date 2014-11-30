6dc4b1384ecfb4181fa2b1c297fdc12ff17c5c55
Tim Harder <radhermit@gmail.com>
2014-11-29 20:43:02 -0800

    disable unshare test until CLONE_NEWUSER is determined supported or not
    CLONE_NEWUSER requires newer glibc/linux support so we should try to
    determine if the system can support it before running the test.

--- test/test_chroot_unshare.py
+++ test/test_chroot_unshare.py
@@ -11,9 +11,9 @@ from chroot.unshare import CLONE_NEWUSER
 
 def test_unshare():
     # most unshare calls require elevated privs, CLONE_NEWUSER doesn't
-    unshare(CLONE_NEWUSER)
+    #unshare(CLONE_NEWUSER)
 
-    # test when running unshare fails
+    # force an unshare failure
     with patch('ctypes.CDLL') as mocked_ctypes:
         mocked_ctypes.unshare = Mock(return_value=1)
         with raises(OSError):
