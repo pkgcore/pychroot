ab39895c127396da8a33bc156bc70ec45e8b7c84
Tim Harder <radhermit@gmail.com>
2016-01-25 21:03:36 -0500

    cli: make docs attribs standalone since they replace help summaries
    From snakeoil-0.6.7 onwards.

--- pychroot/cli.py
+++ pychroot/cli.py
@@ -36,6 +36,8 @@ argparser.add_argument('path', help='path to newroot')
 argparser.add_argument(
     'command', nargs=argparse.REMAINDER, help='optional command to run',
     docs="""
+        Optional command to run.
+
         Similar to chroot(1), if unspecified this defaults to $SHELL from the
         host environment and if that's unset it executes /bin/sh.
     """)
@@ -43,19 +45,21 @@ argparser.add_argument(
     '--no-mounts', action='store_true',
     help='disable the default bind mounts',
     docs="""
-        Use this to obtain a standard chroot environment without any bind
-        mounts that you'd expect when using chroot(1).
+        Disable the default bind mounts which can be used to obtain a standard
+        chroot environment that you'd expect when using chroot(1).
     """)
 argparser.add_argument(
     '--hostname', type=str, help='specify the chroot hostname',
     docs="""
-        In order to set the domain name as well, pass an FQDN instead of a
-        singular hostname.
+        Specify the chroot hostname. In order to set the domain name as well,
+        pass an FQDN instead of a singular hostname.
     """)
 argparser.add_argument(
     '--skip-chdir', action='store_true',
     help="do not change working directory to '/'",
     docs="""
+        Do not change the current working directory to '/'.
+
         Unlike chroot(1), this currently doesn't limit you to only using it
         when the new root isn't '/'. In other words, you can use a new chroot
         environment on the current host system rootfs with one caveat: any
@@ -65,6 +69,8 @@ argparser.add_argument(
     '-B', '--bind', type=bindmount, action=mountpoints,
     metavar='SRC[:DEST]', help='specify custom bind mount',
     docs="""
+        Specify a custom bind mount.
+
         In order to mount the same source to multiple destinations, use the
         SRC:DEST syntax. For example, the following will bind mount '/srv/data'
         to /srv/data and /home/user/data in the chroot::
@@ -81,6 +87,8 @@ argparser.add_argument(
     action=mountpoints, metavar='SRC[:DEST]',
     help='specify custom readonly bind mount',
     docs="""
+        Specify a custom readonly bind mount.
+
         Readonly, recursive bind mounts aren't currently supported on Linux so
         this has to be a standalone option for now. Once they are, support for
         them and other mount attributes will be added as an extension to the
