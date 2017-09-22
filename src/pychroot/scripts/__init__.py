9c7367eb92d6eee034380973ce6c334b71033b50
Tim Harder <radhermit@gmail.com>
2017-09-21 23:31:56 -0400

    pychroot.scripts: fix running pychroot script from the repo

--- src/pychroot/scripts/__init__.py
+++ src/pychroot/scripts/__init__.py
@@ -34,5 +34,5 @@ def run(script_name):
 if __name__ == '__main__':
     # we're in a git repo or tarball so add the base dir to the system path
     repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(0, repo_dir)
+    sys.path.insert(0, os.path.join(repo_dir, 'src'))
     run(os.path.basename(__file__))
