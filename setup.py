f2c04dc7bbc26a4e75045c81b2370563c6c73379
Tim Harder <radhermit@gmail.com>
2020-12-05 00:59:01 -0700

    setup.py: drop old tests_require param
    pkgdist should take care of that transparently now.

--- setup.py
+++ setup.py
@@ -6,24 +6,23 @@ from snakeoil.dist import distutils_extensions as pkgdist
 pkgdist_setup, pkgdist_cmds = pkgdist.setup()
 
 
-setup(**dict(pkgdist_setup,
+setup(**dict(
+    pkgdist_setup,
     description='a python library and cli tool that simplify chroot handling',
     author='Tim Harder',
     author_email='radhermit@gmail.com',
     url='https://github.com/pkgcore/pychroot',
     license='BSD',
     platforms='Posix',
-    tests_require=pkgdist.test_requires(),
     cmdclass=dict(
         pkgdist_cmds,
         test=pkgdist.pytest,
         lint=pkgdist.pylint,
-        ),
+    ),
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        ],
-    )
-)
+    ],
+))
